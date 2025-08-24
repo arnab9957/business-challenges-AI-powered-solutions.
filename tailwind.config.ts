9d4db61a08be877e725faa674c4abc283888f05c
Arnab Kumar Dey <arnabkumardey12062005@gmail.com>
2025-08-24 14:54:43 +0000

    ehen houvaring the generate solution button it is glowing too fast make

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,12 +93,9 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'glow': 'glow 2s ease-in-out infinite',
+        'glow': 'glow 4s ease-in-out infinite',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
-    
-    
